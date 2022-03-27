f0536e4f2868f9fdf22b10fffabd1c909ee2a4a5
dushyant-mishra <dushyant.mishra@gmail.com>
2022-03-27 10:27:22 -0400

    new timestamp function added in common

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,4 +1,5 @@
 import yaml
+import time
 
 
 def read_config(config_path):
@@ -11,3 +12,9 @@ def read_config(config_path):
 #    with open(config_file, "r") as f:
 #        config = yaml.load(f, Loader=yaml.FullLoader)
 #    return config
+
+
+def get_timestamp(name):       #implemented differently using asctime than the get_unique_filename function in utils/model.py
+    timestamp = time.asctime().replace(" ", "_").replace(":", "_")
+    unique_name =  f"{name}_at_{timestamp}"
+    return unique_name
